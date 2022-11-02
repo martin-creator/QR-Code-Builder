1a4f5b5848b94064b01be93befb804e00d479911
martin-creator <martinlubowa@outlook.com>
2022-11-02 14:40:25 +0300

    Created place api

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Place(models.Model):
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=255)
+  image = models.CharField(max_length=255)
+  number_of_tables = models.IntegerField(default=1)
+  font = models.CharField(max_length=100, blank=True)
+  color = models.CharField(max_length=100, blank=True)
+
+  def __str__(self):
+    return "{}/{}".format(self.owner.username, self.name)
--- qrmenu_backend/urls.py
+++ qrmenu_backend/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from core import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+
+    path('api/places/', views.PlaceList.as_view()),
 ]
