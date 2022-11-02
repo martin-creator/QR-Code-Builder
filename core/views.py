097ab2054c5215eadabc4617d3c2cc6806994761
martin-creator <martinlubowa@outlook.com>
2022-11-02 18:51:38 +0300

    Created menu and category apis

--- core/views.py
+++ core/views.py
@@ -2,16 +2,41 @@ from rest_framework import generics
 from . import models, serializers
 
 # Create your views here.
+
+
 class PlaceList(generics.ListCreateAPIView):
-  serializer_class = serializers.PlaceSerializer
+    serializer_class = serializers.PlaceSerializer
 
-  def get_queryset(self):
-    return models.Place.objects.filter(owner_id=self.request.user.id)
+    def get_queryset(self):
+        return models.Place.objects.filter(owner_id=self.request.user.id)
 
-  def perform_create(self, serializer):
-    serializer.save(owner=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-# class PlaceDetail(generics.RetrieveUpdateDestroyAPIView):
-#   permission_classes = [permissions.IsOwnerOrReadOnly]
-#   serializer_class = serializers.PlaceDetailSerializer
-#   queryset = models.Place.objects.all()
+
+class PlaceDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsOwnerOrReadOnly]
+    serializer_class = serializers.PlaceDetailSerializer
+    queryset = models.Place.objects.all()
+
+
+class CategoryList(generics.CreateAPIView):
+    permission_classes = [permissions.PlaceOwnerOrReadOnly]
+    serializer_class = serializers.CategorySerializer
+
+
+class CategoryDetail(generics.UpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [permissions.PlaceOwnerOrReadOnly]
+    serializer_class = serializers.CategorySerializer
+    queryset = models.Category.objects.all()
+
+
+class MenuItemList(generics.CreateAPIView):
+    permission_classes = [permissions.PlaceOwnerOrReadOnly]
+    serializer_class = serializers.MenuItemSerializer
+
+
+class MenuItemDetail(generics.UpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [permissions.PlaceOwnerOrReadOnly]
+    serializer_class = serializers.MenuItemSerializer
+    queryset = models.MenuItem.objects.all()
