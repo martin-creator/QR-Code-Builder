f4073094cef052002e64bcfa9b39865ec0f10f74
martin-creator <martinlubowa@outlook.com>
2022-11-02 10:38:10 +0300

    Revert "Created layout and added notifications with toastify"
    This reverts commit 1e18ed49df87338b7647660c00efec686cbe148a.

--- src/pages/Home.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MainLayout from '../layouts/MainLayout';
-
-const Home = () => <MainLayout>Home</MainLayout>
-
-export default Home;
--- src/pages/Login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MainLayout from '../layouts/MainLayout';
-
-const Login = () => <MainLayout>Login</MainLayout>
-
-
-export default Login;
